6f09d68c7739e83627507a1f17e2de8ffdc91730
Chee <60206746+cheemurakami@users.noreply.github.com>
2024-03-06 05:54:47 +0000

    install mui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Container, Box } from '@mui/material';
+
 export default function Home() {
   return (
-    <p>Home page</p>
+    <>
+      <p>Home page</p>
+      <Container maxWidth="sm">
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </>
   );
 }
