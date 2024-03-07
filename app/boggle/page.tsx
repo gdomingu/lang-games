03cec2c2bc7c6c94f8f7100e7a3aba3363932a47
Chisato Murakami <kiwipom1031@gmail.com>
2024-03-06 20:32:53 -0800

    update eslint and prettier

--- app/boggle/page.tsx
+++ app/boggle/page.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
-import { Box, Button } from '@mui/material';
-import { useState } from 'react';
-import BoggleGrid from '../components/BoggleGrid'
+import { Box, Button } from "@mui/material";
+import { useState } from "react";
+import BoggleGrid from "../components/BoggleGrid";
 
 export default function Boggle() {
-  const [squareGrid, setSquareGrid] = useState<string[][]>([])
+  const [squareGrid, setSquareGrid] = useState<string[][]>([]);
   const CHARS = [
     "A",
     "B",
@@ -33,34 +33,51 @@ export default function Boggle() {
     "X",
     "Y",
     "Z",
-  ]
+  ];
 
   function generate() {
-    let grid = []
+    let grid = [];
     for (let i = 0; i < 4; i++) {
-      let row = []
+      let row = [];
       for (let j = 0; j < 4; j++) {
-        const randomIndex = getRandomIndex()
-        row.push(CHARS[randomIndex])
+        const randomIndex = getRandomIndex();
+        row.push(CHARS[randomIndex]);
       }
-      grid.push(row)
+      grid.push(row);
     }
 
-    setSquareGrid(grid)
+    setSquareGrid(grid);
   }
 
   function getRandomIndex() {
-    return Math.floor(Math.random() * (26))
+    return Math.floor(Math.random() * 26);
   }
 
   return (
     <>
       <h2>Boggle</h2>
-      <Box sx={{ bgcolor: '#cfe8fc', height: '50vh', margin: 2, display: 'flex', flexDirection: 'column' }}>
-        <Box sx={{ bgcolor: '#E2E4F6', height: '30vh', width: '30vh', margin: 'auto' }}>
-          {squareGrid.length > 0 && (<BoggleGrid squareGrid={squareGrid} />)}
+      <Box
+        sx={{
+          bgcolor: "#cfe8fc",
+          height: "50vh",
+          margin: 2,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            bgcolor: "#E2E4F6",
+            height: "30vh",
+            width: "30vh",
+            margin: "auto",
+          }}
+        >
+          {squareGrid.length > 0 && <BoggleGrid squareGrid={squareGrid} />}
         </Box>
-        <Button onClick={generate} variant="contained" sx={{ margin: 'auto' }}>Start</Button>
+        <Button onClick={generate} variant="contained" sx={{ margin: "auto" }}>
+          Start
+        </Button>
       </Box>
     </>
   );
--- app/components/BoggleGrid.tsx
+++ app/components/BoggleGrid.tsx
@@ -1,33 +1,53 @@
-'use client'
+"use client";
 
-import { Box, Button } from '@mui/material';
+import { Box, Button } from "@mui/material";
 
 interface Props {
   squareGrid: string[][];
 }
 
 export default function BoggleGrid(props: Props) {
-  const { squareGrid } = props
+  const { squareGrid } = props;
 
   return (
-    <Box sx={{ bgcolor: '#E7C8DD', height: '30vh', width: '30vh', margin: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
+    <Box
+      sx={{
+        bgcolor: "#E7C8DD",
+        height: "30vh",
+        width: "30vh",
+        margin: "auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+      }}
+    >
       {squareGrid.map((row, i) => {
         return (
           <>
-            <Box key={i} sx={{ margin: 2, display: 'flex', justifyContent: 'space-around' }}>
-              {
-                row.map((char, j) => {
-                  return (
-                    <>
-                      <Button key={j} sx={{ color: '#86626E', width: '4vw', height: '4vh' }}>{char}</Button>
-                    </>
-                  )
-                })
-              }
+            <Box
+              key={i}
+              sx={{
+                margin: 2,
+                display: "flex",
+                justifyContent: "space-around",
+              }}
+            >
+              {row.map((char, j) => {
+                return (
+                  <>
+                    <Button
+                      key={j}
+                      sx={{ color: "#86626E", width: "4vw", height: "4vh" }}
+                    >
+                      {char}
+                    </Button>
+                  </>
+                );
+              })}
             </Box>
           </>
-        )
+        );
       })}
     </Box>
-  )
+  );
 }
