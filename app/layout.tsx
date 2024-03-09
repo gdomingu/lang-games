cf9340a74ce452c2ef1fdd84fa53d5b452546142
Gabe D <gdomingu@github.com>
2024-03-09 15:45:11 -0800

    Adds basic nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import NavLayout from "./NavLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AppRouterCacheProvider>
-          {children}
+          <NavLayout>
+            {children}
+          </NavLayout>
         </AppRouterCacheProvider>
       </body>
     </html>
